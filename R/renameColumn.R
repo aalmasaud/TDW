853fa82c12484d39f1d247446bc5d098d6dc47a6
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-06-26 23:49:03 +0100

    Update renameColumn.R

--- R/renameColumn.R
+++ R/renameColumn.R
@@ -7,8 +7,8 @@ renameColumn <- function(df,old,new) {
   print(old)
   print(new)
   print(df)
-#  if (is.numeric(old))
+ # if (is.numeric(old))
  #   names(df)[old]<-new
  # else
-  #  colnames(df)[colnames(df)==old] <- new
+ #   colnames(df)[colnames(df)==old] <- new
 }
