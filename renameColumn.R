dc39e12df38b48023e0121b39b3483a56f863669
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-06-27 00:32:19 +0100

    Add files via upload

---
+++ renameColumn.R
@@ -0,0 +1,16 @@
+# renameColumn
+#
+#
+
+
+renameColumn <- function(df,old,new) {
+ # print(old)
+#  print(new)
+  # print(df)
+  if (is.numeric(old))
+    names(df)[old]<-new
+  else
+    colnames(df)[colnames(df)==old] <- new
+ # print(df)
+  return(df)
+}
