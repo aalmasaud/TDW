6b7c2a94cd4312301645fcfe97a3ee6422f774b2
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-07-04 19:12:24 +0100

    Add files via upload

--- R/removeColumn.R
+++ R/removeColumn.R
@@ -1,25 +1,8 @@
 #removeColumn
 #
 #
-#removeColumn<-function(dataFile,...) {
-
-# drop <- c(...)
-# dataFile[,!names(dataFile) %in% drop]
-  #return(drop)
-#  return(dataFile)
-
-#}
-
-#removeColumn
-#
-#
 removeColumn<-function(dataFile,...) {
-  x<-list(...)
- # print(...)
-  for (i in 1:length(x)) {
 
-    dataFile[,as.character(x[i])]<- NULL
-  }
-  return(dataFile)
+  df = dataFile[,!names(dataFile) %in% dput(as.character(c(...)))]
 
 }
