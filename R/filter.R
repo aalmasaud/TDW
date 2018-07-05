1999cad78611e3460f474bb63f8aeb42e00cc93e
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-07-06 00:05:05 +0100

    Update filter.R

--- R/filter.R
+++ R/filter.R
@@ -6,10 +6,10 @@
 filter <- function(dataFile,...){
 
 a <-gsub("\r","",paste(...,collapse = ""))
-  d <- gsub(" AND "," & ",a)
-  print(d)
-  dd <- gsub(" OR "," | ",d)
-  print(dd)
+  #d <- gsub(" AND "," & ",a)
+  #print(d)
+  #dd <- gsub(" OR "," | ",d)
+  #print(dd)
   
   #dataFile %>% filter_(dd)
 
