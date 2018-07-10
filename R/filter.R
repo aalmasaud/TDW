6afb4947b267589ffdb4f4436e7f72887aa70096
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-07-10 14:37:50 +0100

    Update filter.R

--- R/filter.R
+++ R/filter.R
@@ -7,10 +7,9 @@ filter <- function(dataFile,...){
   library(data.table)
   d <- gsub(" AND ", " & ", paste(..., collapse = ""))
   dd <- gsub(" OR ", " | ", d)
-  #setDT(dataFile)
-  print(paste0("dataFile[",dd,"]"))
 
-  eval(parse(text=paste0("dataFile[",dd,",]")))
+
+  eval(parse(text=paste0("dplyr::filter(dataFile,",dd,")")))
 #  return(dataFile)
 }
 
