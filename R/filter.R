11b15264dfb4b696facb91e460df5bb5899bcd43
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-07-10 13:09:23 +0100

    Update filter.R

--- R/filter.R
+++ R/filter.R
@@ -9,7 +9,7 @@ filter <- function(dataFile,...){
   dd <- gsub(" OR ", " | ", d)
   setDT(dataFile)
   print(paste0("dt[",dd,"]"))
-  eval(parse(text=paste0("dt[",dd,"]")))
+  #eval(parse(text=paste0("dt[",dd,"]")))
 #  return(dataFile)
 }
 
