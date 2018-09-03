823efac0d1fcdd1a946ca07ff08e28435638976b
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-03 22:55:10 +0100

    Add files via upload

---
+++ inst/www/w1_2.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+  
+  function readFile(ftype, myfile){
+    //disable the button during upload
+    $("#submitbutton").attr("disabled", "disabled");        
+
+	$("#functR").text("readFile");
+    
+    //perform the request
+    var req = ocpu.call("readFile", {
+      type : ftype,
+      file : myfile
+    }, function(session){
+
+		mysession = session;
+
+		$("#functR").text("");
+		$("#functC").text("readFile");
+        $("#key").text(mysession.getKey());
+        $("#location").text(mysession.getLoc());
+      //on success call enrichTimestamp()
+      removeColumns(session);
+    });
+    
+    //if R returns an error, alert the error message
+    req.fail(function(){
+      alert("Server error: " + req.responseText);
+    });
+    
+    //after request complete, re-enable the button 
+    req.always(function(){
+      $("#submitbutton").removeAttr("disabled")
+    });        
+  }    
+  
+  
+  function enrichTimestamp(mydata){
+	$("#functR").text("enrichTimestamp");
+
+	//perform the request
+    var req = ocpu.call("enrichTimedate", {
+      dataFile : mydata,
+	  fromColumn : "Date",
+	  toColumn: "Time",
+	  startDate: $("#startDate").val()
+    }, function(session){
+
+			mysession = session;
+
+		$("#functR").text("");
+		$("#functC").text("enrichTimedate");
+        $("#key").text(mysession.getKey());
+        $("#location").text(mysession.getLoc());
+		
+    }).fail(function(){
+      alert("Server error: " + req.responseText);
+    });        
+  }
+  
+   function removeColumns(mydata){
+	$("#functR").text("removeColumns");
+
+	//perform the request
+    var req = ocpu.call("removeColumn", {
+     "dataFile" : mydata,
+	 "..." : ["Weight__tonnes_","Weight__tonnes_","Flags","Flag_Text","Num_Axles","Axle_Weights__tonnes____","Axle_Spacings__m____"]
+    }, function(session){
+
+			mysession = session;
+
+		$("#functR").text("");
+		$("#functC").text("removeColumns");
+        $("#key").text(mysession.getKey());
+        $("#location").text(mysession.getLoc());
+		      //on success call enrichTimestamp()
+      enrichTimestamp(session);
+    }).fail(function(){
+      alert("Server error: " + req.responseText);
+    });        
+  }
+  
+  $("#submitbutton").on("click", function(){
+    
+    //arguments
+    var ftype = $("#ftype").val();
+    var myfile = $("#fileaddr").val();
+    
+    if(!myfile){
+      alert("No file selected.");
+      return;
+    }
+    
+    readFile(ftype, myfile);        
+  });
+});
