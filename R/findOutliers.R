ebcfc92b7a250a1662f10fc1cbbe5a893ed36bc9
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-03 20:46:32 +0100

    Add files via upload

---
+++ R/findOutliers.R
@@ -0,0 +1,21 @@
+
+
+
+
+findOutliers <- function(dataFile,dataColumn,...) {
+
+aggregate(dataFile[,dataColumn],by=list(newTime=dataFile$newTime,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.25)-IQR(x)*2) -> iqr1
+aggregate(dataFile[,dataColumn],by=list(newTime=dataFile$newTime,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.75)+IQR(x)*2) -> iqr3
+stats<-merge(iqr1,iqr3,by=c("newTime","Lane"))
+
+new <- merge(dataFile,stats,by=c("newTime","Lane"))
+
+names(new)<- gsub("\\.","_",names(new))
+
+rOutliers <- filter2(new,paste(dataColumn," < x_x OR ",dataColumn," > x_y",sep=""))
+rWithoutOutliers <- filter2(new,paste(dataColumn," > x_x AND ",dataColumn," < x_y",sep=""))
+
+return(rWithoutOutliers)
+}
+
+
--- R/readFile.R
+++ R/readFile.R
@@ -6,12 +6,19 @@ library(httr)
 readFile <- function(type,file,...) {
 
   if(grepl("csv$", tolower(type))){
-    read.csv(file,stringsAsFactors=FALSE, ...);
+    data <- read.csv(file,stringsAsFactors=FALSE, ...);
+
   }
   else if (grepl("json", tolower(type))){
     library(jsonlite)
-    fromJSON(file)
+    data <- fromJSON(file)
+  }
+  else if (grepl("xls",tolower(type))){
+    readxl::read_xls(file,skip=6,...)
   }
   else
     stop("input file must be a .csv or JSON file!")
+
+  names(data)<- gsub("\\.","_",name(data))
+  return(data)
 }
