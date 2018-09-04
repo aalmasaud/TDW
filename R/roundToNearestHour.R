1697ef2e82f08e9851dd7cb97b71d52ba363f958
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-04 01:05:31 +0100

    Update roundToNearestHour.R

--- R/roundToNearestHour.R
+++ R/roundToNearestHour.R
@@ -1,10 +1,16 @@
 
 
 roundToNearestHour <- function(dataFile,type,fromColumn,toColumn,...) {
-  if (grepl("trunc$", tolower(type)))
-    dataFile[toColumn] <- substring(trunc(as.POSIXct(dataFile[,fromColumn],usetz = FALSE),"hour"),1,19)
-  else if (grepl("round$", tolower(type)))
-    dataFile[toColumn] <- substring(round(as.POSIXct(dataFile[,fromColumn],usetz = FALSE),"hour"),1,19)
+  if (grepl("trunc$", tolower(type))){
+    res <- substring(trunc(as.POSIXct(dataFile[fromColumn],usetz = FALSE),"hour"),1,19)
+    dataFile[toColumn] <- res
+    }
+
+  else if (grepl("round$", tolower(type))){
+    res <- substring(round(as.POSIXct(dataFile[fromColumn],usetz = FALSE),"hour"),1,19)
+    dataFile[toColumn] <- res
+  }
+
   else
     stop("type must be trunc or round!")
 }
