40d0c16f670b406b7be5d2418c94613e140f2727
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-04 14:01:51 +0100

    Update findOutliers.R

--- R/findOutliers.R
+++ R/findOutliers.R
@@ -4,10 +4,10 @@
 
 findOutliers <- function(mydatafile,dataColumn,...) {
 dataFile <- data.frame(mydatafile)
-iqr1 <- aggregate(dataColumn ~ Hour + Lane,data=dataFile,FUN=function(x) quantile(x, probs = 0.25)-IQR(x)*2,na.rm=T)
-#iqr1 <- aggregate(dataFile[,dataColumn],by=list(newTime=dataFile$newTime,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.25)-IQR(x)*2)
-iqr3 <- aggregate(dataColumn ~ Hour + Lane,data=dataFile,FUN=function(x) quantile(x, probs = 0.75)+IQR(x)*2,na.rm=T)
-#iqr3 <- aggregate(dataFile[,dataColumn],by=list(newTime=dataFile$newTime,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.75)+IQR(x)*2)
+#iqr1 <- aggregate(dataColumn ~ Hour + Lane,data=dataFile,FUN=function(x) quantile(x, probs = 0.25)-IQR(x)*2)
+iqr1 <- aggregate(dataFile[,dataColumn],by=list(Hour=dataFile$Hour,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.25)-IQR(x)*2)
+#iqr3 <- aggregate(dataColumn ~ Hour + Lane,data=dataFile,FUN=function(x) quantile(x, probs = 0.75)+IQR(x)*2)
+iqr3 <- aggregate(dataFile[,dataColumn],by=list(Hour=dataFile$Hour,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.75)+IQR(x)*2)
 stats<-merge(iqr1,iqr3,by=c("Hour","Lane"))
 
 new <- merge(dataFile,stats,by=c("Hour","Lane"))
