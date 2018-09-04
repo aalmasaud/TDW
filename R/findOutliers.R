71b1aa11fa5248464d890ec6130f324437a37f14
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-04 13:45:57 +0100

    Update findOutliers.R

--- R/findOutliers.R
+++ R/findOutliers.R
@@ -4,8 +4,10 @@
 
 findOutliers <- function(mydatafile,dataColumn,...) {
 dataFile <- data.frame(mydatafile)
-iqr1 <- aggregate(dataFile[,dataColumn],by=list(newTime=dataFile$newTime,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.25)-IQR(x)*2)
-iqr3 <- aggregate(dataFile[,dataColumn],by=list(newTime=dataFile$newTime,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.75)+IQR(x)*2)
+iqr1 <- aggregate(dataColumn ~ newTime + Lane,data=dataFile,FUN=function(x) quantile(x, probs = 0.25)-IQR(x)*2)
+#iqr1 <- aggregate(dataFile[,dataColumn],by=list(newTime=dataFile$newTime,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.25)-IQR(x)*2)
+iqr3 <- aggregate(dataColumn ~ newTime + Lane,data=dataFile,FUN=function(x) quantile(x, probs = 0.75)+IQR(x)*2)
+#iqr3 <- aggregate(dataFile[,dataColumn],by=list(newTime=dataFile$newTime,Lane=dataFile$Lane),FUN=function(x) quantile(x, probs = 0.75)+IQR(x)*2)
 stats<-merge(iqr1,iqr3,by=c("newTime","Lane"))
 
 new <- merge(dataFile,stats,by=c("newTime","Lane"))
