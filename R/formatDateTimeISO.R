4a7890bda632c35082b9f2dc6cb48e46d2edbd81
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-12 15:02:21 +0100

    Add files via upload

---
+++ R/formatDateTimeISO.R
@@ -0,0 +1,9 @@
+#formatDateTimeISO
+#
+#
+
+formatDateTimeISO <-function(dataFile,fromColumn,toColumn,...) {
+  
+  res <- substring(strptime(dataFile[[fromColumn]],"%d.%m.%Y %H:%M"),1,19)
+  dataFile[toColumn] <- res
+}
