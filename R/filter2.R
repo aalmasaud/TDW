49a03664a9b5f92b867c326d60240a3a4225f5dd
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-12 18:18:44 +0100

    Update filter2.R

--- R/filter2.R
+++ R/filter2.R
@@ -2,11 +2,17 @@
 #
 #
 
-filter2 <- function(dataFile,...){
+filter2 <- function(dataFile,time,column,...){
+  if (grepl("y$", tolower(time))){
+    require(lubridate)
+    dplyr::filter(dataFile,minute(coulmn) == ...)        
+    }
+  else if (grepl("n$", tolower(time))){
 require(sqldf)
   sqldf(paste0("SELECT * FROM dataFile WHERE ",...))
 
 #  eval(parse(text=paste0("dplyr::filter(dataFile,",dd,")")))
 #  return(dataFile)
+    }
 }
 
