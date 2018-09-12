9ab716495dbd8df32427d350ebff93560988e141
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-12 15:23:09 +0100

    Update formatDateTimeISO.R

--- R/formatDateTimeISO.R
+++ R/formatDateTimeISO.R
@@ -4,6 +4,6 @@
 
 formatDateTimeISO <-function(dataFile,fromColumn,toColumn,...) {
   
-  res <- substring(strptime(dataFile[fromColumn],"%d.%m.%Y %H:%M"),1,19)
+  res <- substring(strptime(dataFile[[,fromColumn]],"%d.%m.%Y %H:%M"),1,19)
   dataFile[toColumn] <- res
 }
