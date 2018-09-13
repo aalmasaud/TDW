83fa5e74398065663b380108a51f1024dadf8008
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-13 13:33:17 +0100

    Update roundToNearestHour.R

--- R/roundToNearestHour.R
+++ R/roundToNearestHour.R
@@ -2,13 +2,13 @@
 
 roundToNearestHour <- function(dataFile,type,fromColumn,toColumn,...) {
   if (grepl("trunc$", tolower(type))){
-    res <- format(substring(trunc(as.POSIXct(dataFile[,fromColumn],usetz = FALSE),"hour"),1,19),format="%Y-%m-%d %H:%M:%S")
+    res <- format(trunc(as.POSIXct(dataFile[,fromColumn],usetz = FALSE),"hour"),format="%Y-%m-%d %H:%M:%S")
     dataFile[toColumn] <- res
     return(dataFile)
     }
 
   else if (grepl("round$", tolower(type))){
-    res <- format(substring(round(as.POSIXct(dataFile[,fromColumn],usetz = FALSE),"hour"),1,19),format="%Y-%m-%d %H:%M:%S")
+    res <- format(round(as.POSIXct(dataFile[,fromColumn],usetz = FALSE),"hour"),format="%Y-%m-%d %H:%M:%S")
     dataFile[toColumn] <- res
     return(dataFile)
   }
