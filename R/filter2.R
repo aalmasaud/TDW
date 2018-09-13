fdeb48f8aa2ae64b44db5547d4e15c4d99c46ead
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-13 12:11:51 +0100

    Update filter2.R

--- R/filter2.R
+++ R/filter2.R
@@ -3,9 +3,15 @@
 #
 
 filter2 <- function(dataFile,...){
+  if(grepl("wtime", tolower(paste0(...)))){
+condition <- "substr(Time,15,2) = '50'"
+  }
+  else 
+    condition <- ...
+require(sqldf)
+  sqldf(paste0("SELECT * FROM dataFile WHERE ",condition))
 
-  require(sqldf)
-  sqldf(paste0("SELECT * FROM dataFile WHERE ",...))
-
+#  eval(parse(text=paste0("dplyr::filter(dataFile,",dd,")")))
+#  return(dataFile)
 }
 
