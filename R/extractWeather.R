edb6d3f8aa47be72745c0d1f510e81e32ea92c34
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-17 01:29:16 +0100

    Update extractWeather.R

--- R/extractWeather.R
+++ R/extractWeather.R
@@ -19,7 +19,7 @@ extractWeather <- function(dataFile,fromColumn,toColumn){
       c[i] <- "Hail"
     if(grepl("snow$", tolower(tempStr)))
       c[i] <- "Snow"
-    if(nchar(c[i] < 1)
+    if(nchar(c[i]) < 1)
        c[i] <- "Normal"
     }
   dataFile[toColumn] <- c
