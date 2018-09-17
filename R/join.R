5641fa155daabf828736a2278cfc2f35c42e7b93
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-17 13:25:52 +0100

    Update join.R

--- R/join.R
+++ R/join.R
@@ -1,11 +1,13 @@
-#join : left join function using two different methods
-#
-#
+#join
+# left join function using two different methods
+# using dplyr package preserves sort, while the second
+# can join using different column names
+
 
 
 join <- function(d,dataFile1,dataFile2,by1,by2,...){
 
-  if (d=="d")
+  if (d=="dplyr")
     dplyr::left_join(dataFile1,dataFile2,by=c(by1))
   else
       merge(dataFile1,dataFile2,by.x=by1,by.y=by2,all.x=TRUE)
