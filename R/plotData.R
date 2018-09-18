3dd7fd9e3ccfd47c44d7f619079158c848906e2d
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-18 23:57:52 +0100

    Add files via upload

---
+++ R/plotData.R
@@ -0,0 +1,29 @@
+#plotData
+#
+#
+
+
+plotData <- function(dataFile,...){
+  c <- NA
+  for(i in 1:nrow(dataFile)) {
+    tempStr <- dataFile[i,Condition]
+    if(nchar(tempStr) < 1)
+      c[i] <- "Normal"
+    else
+      c[i] <- tempStr
+  }
+  
+  avgSpeed <- aggregate(dataFile$Speed__mph_,by=list(Lane=dataFile$Lane,Hour=dataFile$Hour,Condition=dataFile$Condition),FUN=mean)
+  require(ggplot2)
+  t <- ggplot(avgSpeed, aes(factor(Hour), x, fill = Condition)) + 
+    geom_bar(stat="identity", position = "dodge") + 
+    scale_fill_brewer(palette = "Set1") + 
+    theme(axis.text.x = element_text(angle = 90,hjust = 0)) + facet_grid(. ~ Lane) + 
+    labs(title="Hourly Average Speed by Lane", x = "Hour" , y = "Average Speed")
+  #install.packages("plotly")
+  require(plotly)
+  tt <- ggplotly(t)
+  htmlwidgets::saveWidget(as_widget(tt), "visual.html", selfcontained = TRUE)
+  
+    
+}
