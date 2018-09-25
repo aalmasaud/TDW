7431fb1a0741602bbee3fa950829176dd77951fa
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-25 20:28:28 +0100

    Add files via upload

---
+++ R/p.R
@@ -0,0 +1,18 @@
+p <- function(dataFile,...) {
+
+  hourCurrent <- dataFile[1,"Hour"]
+  #hourCurrent <- as.POSIXct( h1)
+  hourEnd <- dataFile[rowCount,"Hour"]
+  #hourEnd <- as.POSIXct( h2)
+  hourComp <- hourCurrent - 3600
+  #print(hourEnd)
+  h <- as.data.frame(hourCurrent)
+  while (hourCurrent <= hourEnd) {
+    if (as.character(hourCurrent,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE) != as.character(hourComp,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE)){
+      h<-rbind(h,hourCurrent)
+    }
+    hourComp <- hourCurrent
+    hourCurrent <- hourCurrent+3600
+  }
+  return(h)
+}
