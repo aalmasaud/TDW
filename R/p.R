a81a3017851461d45c01262ff07ca1fa856b9434
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-26 10:27:30 +0100

    Update p.R

--- R/p.R
+++ R/p.R
@@ -6,12 +6,12 @@ p <- function(dataFile,...) {
   #hourEnd <- as.POSIXct( h2)
   hourComp <- hourCurrent - 3600
   #print(hourEnd)
-  h <- dataFile[1,]
+  h <- dataFile[1,"Hour"]
   while (hourCurrent <= hourEnd) {
     if (as.character(hourCurrent,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE) != as.character(hourComp,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE)){
     Tt <- filter2(dataFile,paste0('Hour == ','"',as.character(hourCurrent,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE),'"',sep=""))
 
-      h<-rbind(h,Tt)
+      h<-rbind(h,Tt$Hour)
     }
     hourComp <- hourCurrent
     hourCurrent <- hourCurrent+3600
