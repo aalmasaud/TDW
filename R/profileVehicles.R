c5599bf9e18a256f31641f55125e1126bf231d64
aalmasaud <abdullah.k.almasaud@gmail.com>
2018-09-26 09:42:17 +0100

    Update profileVehicles.R

--- R/profileVehicles.R
+++ R/profileVehicles.R
@@ -18,7 +18,8 @@ profileVehicles <-function(dataFile,...) {
   while (hourCurrent <= hourEnd) {
     if (as.character(hourCurrent,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE) != as.character(hourComp,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE)){
     #print(as.character(hourCurrent,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE))
-    Tt <- filter2(dataFile,paste0('Hour == "',as.character(hourCurrent,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE),'"',sep=""))
+    Tt <- filter2(d,paste0('Hour == ','"',hourCurrent,'"',sep=""))  
+    #Tt <- filter2(dataFile,paste0('Hour == "',as.character(hourCurrent,format = "%Y-%m-%d %H:%M:%S",usetz=FALSE),'"',sep=""))
     weather <- Tt[1,"Condition"]
 
 
